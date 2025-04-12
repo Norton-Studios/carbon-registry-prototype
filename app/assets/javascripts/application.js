74724ca397571aa0dfdb530dee923a9c3fac3a8f
mandy <mcyeekyun@gmail.com>
2025-04-12 21:25:57 +0800

    add registry page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,5 +4,18 @@
 //
 
 window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-})
+  const menuItems = document.querySelectorAll('.ds_site-navigation__link');
+  const currentPath = window.location.pathname;
+
+  menuItems.forEach(item => {
+    const itemPath = item.getAttribute('href');
+
+    if (itemPath === currentPath) {
+      item.classList.add('ds_current');
+      item.setAttribute('aria-current', 'page');
+    } else {
+      item.classList.remove('ds_current');
+      item.removeAttribute('aria-current');
+    }
+  });
+});
--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,39 @@
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+const govukPrototypeKit = require('govuk-prototype-kit');
+const router = govukPrototypeKit.requests.setupRouter();
+const path = require('path');
+const fs = require('fs');
 
+function loadProjectsData() {
+  const dataPath = path.join(__dirname, '/assets/data/projects.json');
+  return JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+}
+
+router.get('/registry', (_, res) => {
+  const projects = loadProjectsData();
+  res.render('registry', { projects });
+});
+
+router.get('/projects/:name', (req, res) => {
+  const projects = loadProjectsData();
+  const projectName = req.params.name;
+
+  const project = projects.find(
+    p => p.name.toLowerCase().replace(/\s+/g, '-') === projectName.toLowerCase()
+  );
+
+  if (project) {
+    res.render('projects/project-details', { project });
+  } else {
+    res.status(404).redirect('/error-page-not-found');
+  }
+});
+
+router.get('/', (_, res) => {
+  res.redirect('/registry');
+});
+
+module.exports = router;
 // Add your routes here
